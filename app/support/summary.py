e90f1c0921aac0d5830f68f6c377aeceac21195e
Sid Ganesan <siddha.ganesan@gmail.com>
2023-03-17 10:58:23 +0000

    Add analyse graphs for CLI command

--- app/support/summary.py
+++ app/support/summary.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 from rich.console import Console
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import matplotlib.dates as mdates
 import app.models as m
 
 from datetime import datetime
@@ -41,10 +44,33 @@ def all_analysis() -> None:
     df = pd.read_sql_query(query, connection)
     # month on month change in web vists
 
-    frames = []
-    grouped_by_site = df.groupby("website")
-    for _, site_df in grouped_by_site:
-        frames.append(_monthly_growth(site_df))
+    save_path = Path("app") / "local" / "output"
+    if not save_path.exists():
+        save_path.mkdir(parents=True)
 
-    growth_df = pd.concat(frames)
-    console.print(growth_df)
+    grouped_by_site = df.groupby("website")
+    for site, site_df in grouped_by_site:
+        site_df = _monthly_growth(site_df)
+        plt.bar(site_df.index, site_df.mom_traffic_growth, width=3)
+        plt.title(f"Monthly change in traffic for {site}", fontsize=20)
+
+        # set axis lines
+        plt.axhline(y=0, color='k', linestyle='-')
+        plt.axhline(y=site_df.mom_traffic_growth.mean(), color='r', linestyle='-.', linewidth=1, label="Avg Growth")
+
+        # Percentage Formatting
+        plt.ylabel("Percentage Growth (%)", fontsize=12)
+        y = plt.gca().yaxis 
+        y.set_major_formatter(mticker.PercentFormatter())
+        
+        # Month formatting
+        plt.xlabel("Month", fontsize=20)
+        x = plt.gca().xaxis
+        x.set_major_locator(mdates.MonthLocator())
+        x.set_major_formatter(mdates.DateFormatter("%b %y"))
+
+        name = str(site).split(".")[0]
+        save_path = Path("app") / "local" / "output" / f"{name}.png"
+        log.info("Saving graphs", path=save_path)
+        plt.savefig(save_path)
+        
